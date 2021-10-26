af5464bbe2eae7318f8efd4d6fb9885a228a303a
Mohamed Taieb Slama <mohamedtaiebslama@gmail.com>
2021-10-26 15:51:20 +0100

    adding singleton Background Music + Creating Service Script

---
+++ Frontend_unity/Blackjack_Reinforcement_Learning/Assets/Scripts/Music.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(AudioListener))]
+[RequireComponent(typeof(AudioSource))]
+public class Music : MonoBehaviour {
+
+    private static Music instance = null; // Singleton Instance of Music
+    public static Music Instance // the instance's property
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (Music)FindObjectOfType(typeof(Music));
+            }
+            return instance;
+        }
+    }
+
+    /*
+     * Keep Game Object when going between Scenes.
+     */
+    private void Awake()
+    {
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
